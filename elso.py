4eeee031fe83ea30d50dbf16d1ed064d7f2f0ffb
Gyorshalal12457896 <markus.rego.benedek@szbiszeged.hum>
2022-09-19 09:31:19 +0200

    masodik commit

--- elso.py
+++ elso.py
@@ -4,3 +4,11 @@ system("cls")
 elsovaltozo = 12
 print(elsovaltozo)
 
+elsovaltozo = 12.32
+print(type(elsovaltozo))
+
+elsovaltozo = True
+print(type(elsovaltozo))
+
+elsovaltozo = [1,2]
+print(type(elsovaltozo))
